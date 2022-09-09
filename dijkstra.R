badaf74e6327881373348508c14a156cf7d8bcba
kiriakospapa <kyriakospapadopoulosauto@gmail.com>
2022-09-09 18:11:21 +0300

    Closest Node found. It's the variable `closest_node`

--- dijkstra.R
+++ dijkstra.R
@@ -2,15 +2,24 @@ dijkstra <-function(data, init_node){
   if(is.data.frame(data)==TRUE & init_node %in% data[,1]){
     
     ##setup
+    
     allnodes<-data[,1]  #take every elements from first column of input data
+    
     unvisited<-allnodes[!duplicated(allnodes)] #find out how many different elements in first column of data
+    
     visited<-c() #create a empty vector to store visited nodes later on
+    
     init_nodevector<-rep(init_node,length(unvisited)) #create a list of init_noed with the length of every different nodes (for creating path dataframe)
+    
     path_length<-rep(Inf,length(unvisited))
+    
     previous_nodes<-rep(NA,length(unvisited))
+    
     everynodes<-allnodes[!duplicated(allnodes)] #Use to create path dataframe. In order not to confused with unvisited vector.
+    
+    
     pathdata<-data.frame(init_nodevector,everynodes,previous_nodes,path_length) #create a path dataframe to make calculation easier to see
-   # print(pathdata)
+   
     
     
     #find the index of nodes which next to init_node
@@ -27,23 +36,38 @@ dijkstra <-function(data, init_node){
     length_of_nodes_next_to_init <- data[neighbor_index_init,3] #get the length data from data(wiki_graph)
     
     
-    neighbor_index_init<- which(data[,1]==init_node) #find the row index of init_node
-    node_nextto_init <-data[neighbor_index_init,2] #find the node next to init_node
-
-    distances = data[neighbor_index_init, 3]
-    print(distances)
-
-    # while(length(unvisited) > 0 ){
-    # 
-    #     neighbor_index_init<- which(allnodes %in% init_node) #find the row index of init_node
-    # 
-    #     print(neighbor_index_init)
-    # }
+    neighbor_index_of_current_node<- which(data[,1]==init_node) #find the row index of init_node
+    nodes_nextto_current_node <-data[neighbor_index_init,2] #find the node next to init_node
+    first_index_of_current_node <- neighbor_index_init[1]
     
+    index_closest_node_to_current_node <- first_index_of_current_node + min(data[neighbor_index_of_current_node,2]) # min returns the index
+    closest_node_to_current_node <- first_index_of_current_node + which(data[neighbor_index_of_current_node,3] == min(data[neighbor_index_of_current_node,3])) -1 # The variable contains the number of the closest node
+
+    distances = data[neighbor_index_init, 2]
+    closest_node = data[closest_node_to_current_node, 2]
+
+     while(length(unvisited) > 0 ){
+       
+       
+    
+        #neighbor_index_init<- which(allnodes %in% init_node) #find the row index of init_node
+         
+          # print("first index of closest node is ")
+          # print(first_index_of_current_node)
+          # print("neightbour index is")
+          # print(neighbor_index_init)
+          # print("the minimum is")
+          # print(which(data[neighbor_index_of_current_node,3] == min(data[neighbor_index_of_current_node,3])))
+          #print(closest_node_to_current_node)
+          print(closest_node)
+      }
+
     
      
       
+  pick_the_smallest_value <- function(x){
     
+  }  
     
   
   }
