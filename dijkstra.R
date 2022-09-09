44c9277ac71bc9d29243fa89cf92de62181739d0
Your Name <you@example.com>
2022-09-09 21:42:16 +0800

    check distance

--- dijkstra.R
+++ dijkstra.R
@@ -26,26 +26,26 @@ dijkstra <-function(data, init_node){
       #Step2 calculate the path length next to init_node
     length_of_nodes_next_to_init <- data[neighbor_index_init,3] #get the length data from data(wiki_graph)
     
-    i<-1
-    neighbor_index_init<- which(initnode %in% init_node) #find the row index of init_node
+    
+    neighbor_index_init<- which(data[,1]==init_node) #find the row index of init_node
     node_nextto_init <-data[neighbor_index_init,2] #find the node next to init_node
 
     distances = data[neighbor_index_init, 3]
+    print(distances)
 
-
-    while(length(unvisited) == 0 ){
-
-        neighbor_index_init<- which(allnodes %in% init_node) #find the row index of init_node
-
-
-    }
+    # while(length(unvisited) > 0 ){
+    # 
+    #     neighbor_index_init<- which(allnodes %in% init_node) #find the row index of init_node
+    # 
+    #     print(neighbor_index_init)
+    # }
     
     
-     print(length_of_nodes_next_to_init)  
+     
       
     
     
-   print(pathdata)   
+  
   }
     else{stop()}
     
@@ -68,4 +68,4 @@ wiki_graph <-data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
                    v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
                    w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
 
-dijkstra(wiki_graph,2)
+dijkstra(wiki_graph,1)
