0765d689a9b1531bec6b60989aba53cfad373064
Your Name <you@example.com>
2022-09-10 03:50:04 +0800

    change to latest version after 0910 library session

--- dijkstra.R
+++ dijkstra.R
@@ -1,4 +1,15 @@
 dijkstra <-function(data, init_node){
+  
+  
+  pick_the_smallest_value <- function(subvector, i){
+    
+    sorted_subvector <- sort(subvector)
+    i_smallest_value <- sorted_subvector[i]
+    
+    return(which(i_smallest_value == sorted_subvector))
+    
+  }  
+  
   if(is.data.frame(data)==TRUE & init_node %in% data[,1]){
     
     ##setup
@@ -18,8 +29,9 @@ dijkstra <-function(data, init_node){
     everynodes<-allnodes[!duplicated(allnodes)] #Use to create path dataframe. In order not to confused with unvisited vector.
     
     
+    
     pathdata<-data.frame(init_nodevector,everynodes,previous_nodes,path_length) #create a path dataframe to make calculation easier to see
-   
+    
     
     
     #find the index of nodes which next to init_node
@@ -28,14 +40,14 @@ dijkstra <-function(data, init_node){
     
     
     #Calculation
-      #Step1 set the path length to init_node=0
+    #Step1 set the path length to init_node=0
     init_index_ineverynodes<- which(everynodes %in% init_node)
     pathdata[init_index_ineverynodes, 4] <- 0
     pathdata[init_index_ineverynodes, 3] <- init_node
     visited <-append(visited,init_node)
     # print(pathdata)
     
-      #Step2 calculate the path length next to init_node
+    #Step2 calculate the path length next to init_node
     length_of_nodes_next_to_init <- data[neighbor_index_init,3] #get the length data from data(wiki_graph)
     
     
@@ -43,43 +55,89 @@ dijkstra <-function(data, init_node){
     nodes_nextto_current_node <-data[neighbor_index_init,2] #find the node next to init_node
     first_index_of_current_node <- neighbor_index_init[1]
     
-    index_closest_node_to_current_node <- first_index_of_current_node + min(data[neighbor_index_of_current_node,2]) # min returns the index
+    #index_closest_node_to_current_node <- first_index_of_current_node + min(data[neighbor_index_of_current_node,2]) # min returns the index
     closest_node_to_current_node_index <- first_index_of_current_node + which(data[neighbor_index_of_current_node,3] == min(data[neighbor_index_of_current_node,3])) -1 # The variable contains the number of the closest node
-
+    
     distances = data[neighbor_index_init, 2]
     closest_node = data[closest_node_to_current_node_index, 2]
-    visited <-append(visited,closest_node)
-   
+    visited <- append(visited, closest_node)
+    
     closest_node_index_in_pathdata<- which(everynodes %in% closest_node) #find the index of the closet node in pathdata
     
     pathdata[closest_node_index_in_pathdata,4]<- data[closest_node_to_current_node_index,3] #update the length 
     pathdata[closest_node_index_in_pathdata,3]<- closest_node # update the previous node
-    print(pathdata)
+    # unvisited[closest_node_to_current_node_index] <- NULL
+    #unvisited <- within(unvisited, rm(closest_node))
+    unvisited <- unvisited[-init_node]
+    unvisited <- unvisited[- which( closest_node == unvisited)]
+    print(visited)
+    # node_nextto_init <-data[neighbor_index_init,2] #find the node next to init_node
     
-    
-    
-    
-    
-    # print(visited)
-     while(length(unvisited) > 0 ){
-       
-       
-          print(closest_node)
-       break
-      }
-
-    
-     
+    i<-1
+    while(length(unvisited) > 0 ){
+      neighbor_index_init<- which(allnodes %in% closest_node) #find the row index of init_node
       
-  pick_the_smallest_value <- function(x){
+      neighbor_index_of_current_node<- which(data[,1]==closest_node) 
+      nodes_nextto_current_node <-data[neighbor_index_of_current_node,2] #find the node next to init_node
+      first_index_of_current_node <- neighbor_index_init[1]
+      
+      #index_closest_node_to_current_node <- first_index_of_current_node + min(data[neighbor_index_of_current_node,2]) # min returns the index
+      closest_node_to_current_node_index <- first_index_of_current_node + which(data[neighbor_index_of_current_node,3] == min(data[neighbor_index_of_current_node,3])) -1 # The variable contains the number of the closest node
+      distances = data[neighbor_index_init, 2]
+      closest_node = data[closest_node_to_current_node_index, 2]
+      print(pathdata)
+      
+      repeat{
+        stop <- FALSE
+        check<- (!(closest_node %in% visited))
+        print(typeof(check))
+        print(check)
+        
+        for (boolean_value in check){
+          if (boolean_value == FALSE){
+            stop<-TRUE
+            break
+          } 
+        }
+        if (stop){
+          break
+        }
+        print("tried to change the closest node")
+        i <- i + 1
+        closest_node_to_current_node_index <- first_index_of_current_node + pick_the_smallest_value(data[neighbor_index_of_current_node, 3]) -1 # The variable contains the number of the closest node
+        distances = data[neighbor_index_init, 2]
+        closest_node = data[closest_node_to_current_node_index, 2]
+        
+        
+        
+        
+      }
+      ## WE NEED TO CHECK OUT IF THE CLOSEST NODE IS IN UNVISITED VECTOR
+      
+      visited <-append(visited,closest_node)
+      
+      closest_node_index_in_pathdata<- which(everynodes %in% closest_node) #find the index of the closet node in pathdata
+      
+      pathdata[closest_node_index_in_pathdata,4]<- data[closest_node_to_current_node_index,3] #update the length 
+      pathdata[closest_node_index_in_pathdata,3]<- closest_node # update the previous node
+      # unvisited[closest_node_to_current_node_index] <- NULL
+      #unvisited <- within(unvisited, rm(closest_node))
+      print("the closest node is")
+      print(closest_node)
+      unvisited <- unvisited[- which( closest_node == unvisited)]
+      print(pathdata)
+      print(unvisited)
+      
+      
+    }
     
-  }  
     
+    
+    
+  }
+  else{stop()}
   
-  }
-    else{stop()}
-    
-  }
+}
 
 
 
@@ -95,7 +153,7 @@ dijkstra <-function(data, init_node){
 
 ###testing
 wiki_graph <-data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),
-                   v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
-                   w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
+                        v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
+                        w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
 
-dijkstra(wiki_graph,2)
+dijkstra(wiki_graph,1)
