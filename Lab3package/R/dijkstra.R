8bca119dfb51ad44e9aa368c4cd7563bb649c928
Your Name <you@example.com>
2022-09-11 20:15:03 +0800

    Fix warnings

--- Lab3package/R/dijkstra.R
+++ Lab3package/R/dijkstra.R
@@ -1,11 +1,16 @@
 #' Takes a graph and an initial node, then calculates the shortest path from the initial node to every other node in the graph.
 #' 
 #' @param data A data.frame with three variables (v1, v2 and w) that contains the nodes of the graph (from v1 to v2) with the length of the node (w)
-#' @param intial_node The node to start with.
+#' @param initial_node The node to start with.
+#' @usage dijkstra(data, initial_node)
 #' @return The shortest path from the initial node to every other node.
+#' @export 
 #' @examples
-#' dijkstra(wiki_graph, 1)
-#' dijkstra(wiki_graph, 3)
+#' dijkstra(data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)), 1)
+#' dijkstra(data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)), 3)
+#' @seealso [Dijkstra algorithm on wiki](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
+
+
 dijkstra <-function(data, initial_node){
   
   
