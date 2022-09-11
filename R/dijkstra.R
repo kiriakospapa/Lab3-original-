6dce50a2a6d32ba91a8bce29bbd27a797b2a6421
Your Name <you@example.com>
2022-09-12 04:08:16 +0800

    Haven't add wiki_graph yet

--- R/dijkstra.R
+++ R/dijkstra.R
@@ -5,9 +5,6 @@
 #' @usage dijkstra(data, initial_node)
 #' @return The shortest path from the initial node to every other node.
 #' @export 
-#' @examples
-#' dijkstra(data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)), 1)
-#' dijkstra(data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)), 3)
 #' @seealso [Dijkstra algorithm on wiki](https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm)
 
 
@@ -146,7 +143,6 @@ dijkstra <-function(data, initial_node){
       
       
       unvisited <- unvisited[- which( closest_node == unvisited)]
-      return
       
     }
     
